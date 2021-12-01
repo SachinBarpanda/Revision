02a17e489f01be84c003625028e1ac2841106040
SachinBarpanda <sachinongithub@gmail.com>
2021-12-01 23:54:14 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (char c=0x0900;c<=0x0970 ; c++) {
+            System.out.print(c+" ");
+        }
+    }
+}
