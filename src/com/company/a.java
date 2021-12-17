770d936806c9f88f31480ffacbe0f26c41291c73
SachinBarpanda <sachinongithub@gmail.com>
2021-12-17 19:51:24 +0530

    1 lesson

--- src/com/company/a.java
+++ src/com/company/a.java
@@ -2,7 +2,12 @@ package com.company;
 
 public class a {
     public static void main(String[] args) {
-        double s = 12.7% 3.1;
-        System.out.println(s);
+        double s = 12.7 % 3.1;
+//        System.out.println(s);
+        short a = 4;
+        byte b = 3;
+//        System.out.println(((Object)(a+b)).getClass().getSimpleName());
+        System.out.println(4/3*5);
+
     }
 }
